49c49e77f5817ba4431826ca9614132b20f757fa
SPHsofi <Sofia_pash@mail.ru>
2023-12-14 18:21:52 +0300

    feat: 5 (removeLeadingAndTrailingWhitespaces) task solved

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -85,8 +85,9 @@ function getFirstChar(/* value */) {
  *   removeLeadingAndTrailingWhitespaces('cat ') => 'cat'
  *   removeLeadingAndTrailingWhitespaces('\t\t\tHello, World! ') => 'Hello, World!'
  */
-function removeLeadingAndTrailingWhitespaces(/* value */) {
-  throw new Error('Not implemented');
+function removeLeadingAndTrailingWhitespaces(value) {
+  return value.trim();
+  // throw new Error('Not implemented');
 }
 
 /**
