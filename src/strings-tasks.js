5960f0ae367b9d37db982827828525bea30c336a
SPHsofi <Sofia_pash@mail.ru>
2023-12-15 15:12:21 +0300

    feat: 21 (reverseWords) task solved

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -387,8 +387,12 @@ function findLongestWord(sentence) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  const s = str.split(' ');
+  const m = s.map((word) => word.split('').reverse().join(''));
+  const res = m.join(' ');
+  return res;
+  // throw new Error('Not implemented');
 }
 
 /**
